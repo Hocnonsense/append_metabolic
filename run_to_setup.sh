be8e4f044de0abbe1c9ab9b46b84c656d84f2a8c
hwrn.aou <hwrn.aou@sjtu.edu.cn>
2023-11-16 22:11:46 +0800

    fix: update install

--- run_to_setup.sh
+++ run_to_setup.sh
@@ -2,18 +2,16 @@ tar zxvf Accessory_scripts.tgz; rm Accessory_scripts.tgz
 tar zxvf METABOLIC_hmm_db.tgz;  rm METABOLIC_hmm_db.tgz
 tar zxvf METABOLIC_template_and_database.tgz; rm  METABOLIC_template_and_database.tgz
 tar zxvf Motif.tgz; rm Motif.tgz
-mkdir kofam_database  
-cd kofam_database  
+mkdir kofam_database
+cd kofam_database
 curl --silent ftp://ftp.genome.jp/pub/db/kofam/ko_list.gz  -O ko_list.gz
 curl --silent ftp://ftp.genome.jp/pub/db/kofam/profiles.tar.gz  -O profiles.tar.gz
-gzip --quiet -d ko_list.gz  
-tar xzf profiles.tar.gz; rm profiles.tar.gz  
-mv ../All_Module_KO_ids.txt profiles
-cd profiles  
-cp ../../Accessory_scripts/batch_hmmpress.pl ./  
-perl batch_hmmpress.pl
-cd ../
-cd ../
+gzip --quiet -d ko_list.gz
+tar xzf profiles.tar.gz; rm profiles.tar.gz
+ln -s ../../All_Module_KO_ids.txt profiles
+cd profiles
+perl ../../Accessory_scripts/batch_hmmpress.pl
+cd ../../
 mkdir dbCAN2
 cd dbCAN2
 wget https://bcb.unl.edu/dbCAN2/download/Databases/dbCAN-old@UGA/dbCAN-fam-HMMs.txt.v10  -O dbCAN-fam-HMMs.txt
